d8bc5043d7ace96d1d2e299d88b348ddecc71796
sookim-1 <scstnghks@gmail.com>
2024-10-25 14:10:52 +0900

    test : socketcluter - test case 추가

--- Tests/SkSocketSwiftTests/SkSocketSwiftTests.swift
+++ Tests/SkSocketSwiftTests/SkSocketSwiftTests.swift
@@ -3,67 +3,34 @@ import Testing
 
 struct SkSocketSwiftTests {
 
-    @Test("초기화 테스트")
-    func initSkSocketClient() async throws {
-        let localhostAddress = "ws://127.0.0.1:8080/channel"
-        let client = SkSocketClient(url: papahostAddress)
-        let counter = AtomicInteger()
-
-        client.connect()
-
-        #expect(client.isConnected())
-
-        do {
-            try client.subscribe(channelName: "dw")
-        } catch {
-            print("🚨 \(error.localizedDescription)")
-        }
-
-        do {
-            try client.unsubscribe(channelName: "vv")
-        } catch {
-            print("🚨 \(error.localizedDescription)")
-        }
-
-        client.setBasicListener { client in
-            client.socket?.receive(completionHandler: { result in
-                switch result {
-                case .success(let success):
-                    print(success)
-                case .failure(let failure):
-                    print(failure.localizedDescription)
-                }
-            })
-        } onConnectError: { client, error in
-            client.socket?.receive(completionHandler: { result in
-                switch result {
-                case .success(let success):
-                    print(success)
-                case .failure(let failure):
-                    print(failure.localizedDescription)
-                }
-            })
-        } onDisconnect: { client, error in
-            client.socket?.receive(completionHandler: { result in
-                switch result {
-                case .success(let success):
-                    print(success)
-                case .failure(let failure):
-                    print(failure.localizedDescription)
-                }
-            })
-        }
-
-        print("👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉👉")
-        #expect(client.socket == nil)
-    }
-
     @Test("JSON String 변환테스트")
     func convertJSONString() async throws {
         let counter = AtomicInteger()
         let sampleEmitEvent = EmitEvent(event: "#subscribe", data: AuthChannel(channel: "helloWorld"), cid: counter.incrementAndGet())
 
         #expect(sampleEmitEvent.toJSONString()! == nil)
+    }
+
+    @Test("localhost 테스트")
+    func testLocalHostItem() async throws {
+        let localhostAddress = "ws://127.0.0.1:8080/channel"
+        let client = SkSocketClient(url: localhostAddress)
+        client.connect()
+
+        client.setBasicListener(onConnect: {
+            Task {
+                do {
+                    for try await message in client.receiveSocket() {
+                        let str = String(decoding: message, as: UTF8.self)
+                        #expect(str == nil)
+                    }
+                } catch {
+                    #expect(error == nil)
+                }
+            }
+        }, onDisconnect: { 
+            client.connect()
+        })
     }
 
 }
